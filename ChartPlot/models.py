299a5b60bfcdcbf7391940f6542b6bd7cb880602
Dallasz <grave-theboss@hotmail.com>
2021-12-13 19:32:20 -0300

    templatesUpdated

--- ChartPlot/models.py
+++ ChartPlot/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ChartPlot(models.Model):
     name = models.CharField(max_length=255)
     events = models.TextField(max_length=10000)
+    date_created = models.DateField(auto_now_add=True)
     user = models.ForeignKey('User.User', on_delete=models.CASCADE)
     
     def __str__(self):
