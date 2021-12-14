6079a70af68a5f3417f9bae645e8390482ab936b
Dallasz <grave-theboss@hotmail.com>
2021-12-14 12:05:52 -0300

    ok

--- ChartPlotChallenger/settings.py
+++ ChartPlotChallenger/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!lerkkxi_lz8@-y=lt(-b37+)(*g034c*gww18&z@g9a87yud%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://chartplot-challenger.herokuapp.com"]
 
 
 # Application definition
