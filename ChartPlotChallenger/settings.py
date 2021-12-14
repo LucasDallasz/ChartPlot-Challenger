c5c491f5df21d60f7d33f40de7fd7c1c36e42d84
Dallasz <grave-theboss@hotmail.com>
2021-12-14 11:53:17 -0300

    LibDjangoOnHerokuAddied

--- ChartPlotChallenger/settings.py
+++ ChartPlotChallenger/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'User.User'
+
+django_on_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ChartPlotChallenger.wsgi  --log-file -
